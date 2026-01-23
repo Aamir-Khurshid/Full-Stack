30a61458d0d43d4bb0e5ad24d18040660f5ff068
Aamir Khurshid <108178409+Aamir-Khurshid@users.noreply.github.com>
2026-01-23 23:40:02 +0530

    Add files via upload

---
+++ JavaScript/experimentOne.js
@@ -0,0 +1,87 @@
+// const a = [1,2];
+// a.push(2)
+// console.log(a)
+
+
+
+// const obj = {};
+
+// obj.name= "aamir"
+
+// console.log(obj)
+
+
+
+
+// Import express
+
+
+// const express = require('express');
+// const app = express();
+// const port = 3000;
+
+// // Sample data
+// const users = [
+//   { id: 1, name: "Aamir" },
+//   { id: 2, name: "Ali" },
+//   { id: 3, name: "Sara" }
+// ];
+
+// // Routes
+// app.get('/', (req, res) => {
+//   res.send('Welcome to my simple API!');
+// });
+
+// app.get('/users', (req, res) => {
+//   res.json(users); // return JSON data
+// });
+
+// // Start server
+// app.listen(port, () => {
+//   console.log(`Server running at http://localhost:${port}`);
+// });
+
+
+// const a;
+// console.log(a)
+
+
+
+
+// function any(){
+//     var x = 20;
+//     if(true){
+//         const x = 30;
+//         console.log(x)
+//     }
+//     console.log(x)
+// }
+// any()
+
+ function any(){
+     let x = 20;
+     if(true){
+         x = 30;
+         console.log(x)
+     }
+     console.log(x)
+ }
+ any()
+
+
+// console.log('3'- 1)
+// console.log(null + 1)
+// console.log(undefined + 1)
+// console.log(true+false)
+// console.log(typeof null )
+// console.log(typeof undefined )
+// console.log(typeof [] )
+// console.log(typeof 123n)
+
+// console.log(Boolean(0))
+// console.log(Boolean('0'))
+// console.log(Boolean([]))
+// console.log(Boolean(undefined))
+
+console.log("5"==5)
+console.log("5"===5)
