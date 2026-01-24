6ae7f728999e12bebd4ca8114b36942eb607f8dc
Aamir Khurshid <108178409+Aamir-Khurshid@users.noreply.github.com>
2026-01-24 20:38:35 +0530

    Add files via upload

---
+++ JavaScript/experimentFour.js
@@ -0,0 +1,49 @@
+// Play with Function
+
+
+// Function Declaration
+// function hello(name){
+//     console.log(`Hello ${name}`)
+// }
+// hello('Aamir')
+
+
+
+// Function Expression
+// let func = function(){
+// }
+
+
+
+// Fat arrow function
+// ()=>{
+// }
+
+
+// function greet (name){      // name is parameter
+// }
+// greet(Aamir)              // Aamir is argument
+
+
+
+
+// function add(one = 1,two = 2){        // Default Value
+//     console.log(one+two)
+// }
+
+// add(2)
+
+
+
+// function add(one = 1,two = 2,...rest){        // Rest Value
+//     console.log(rest)
+// }
+
+// add(2,3,4,5,5,66,6,6,6)
+
+
+// IIFE
+// (function(){
+//     console.log('hehe')
+// })();
+
