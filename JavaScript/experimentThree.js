09efbe065de22ca085290fea9ab4793a7d1eac86
Aamir Khurshid <108178409+Aamir-Khurshid@users.noreply.github.com>
2026-01-24 12:36:05 +0530

    Add files via upload

---
+++ JavaScript/experimentThree.js
@@ -0,0 +1,36 @@
+// var sum = 0;
+// for(let i = 0;i<5;i++){
+//  sum += i
+// }
+// console.log(sum)
+
+// for (let i = 1; i < 50; i++) {
+//     if (i % 3 === 0) {
+//         console.log(i)
+//     }
+// }
+
+// for (let i = 1; i < 100; i++) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//         console.log(i)
+//     }
+// }
+
+
+
+// for (let i = 1; i < 100; i++) {
+
+//     if (i % 7 === 0) {
+//         break
+//     }
+//     console.log(i)
+// }
+
+
+// for (let i = 1; i < 100; i++) {
+
+//     if (i % 3 === 0) {
+//         continue
+//     }
+//     console.log(i)
+// }
---
+++ JavaScript/experimentTwo.js
@@ -0,0 +1,41 @@
+// if(12>13 || 13<14){
+//     console.log('🤯')
+// }
+// if(12>13 && 13<14){
+//     console.log('🥵')
+// }
+
+// console.log(5 && 10)   
+// console.log(0 && 10)
+// console.log(5 || null)
+
+
+// 12<13 ? console.log('yes'):console.log('no')
+
+// console.log(typeof [])
+
+// function getGrade(score){
+//     if(score>=90){
+//         console.log('A+')
+//     }
+//     else if(score>=80 && score<=89){
+//         console.log('A')
+//     }
+//     else if(score>=70 && score<=79){
+//         console.log('B')
+//     }
+//     else if(score>=60 && score<=69){
+//         console.log('C')
+//     }
+//     else if(score>=33 && score<=59){
+//         console.log('D')
+//     }
+//     else if(score>=0 && score<=32){
+//         console.log('Fail')
+//     }
+//     else{
+//         console.log('WTH')
+//     }
+// }
+
+// getGrade(45)
