6b33855ae0e3d8517729e83763aca37e008e6d60
Aamir Khurshid <108178409+Aamir-Khurshid@users.noreply.github.com>
2026-01-26 11:28:12 +0530

    Add files via upload

---
+++ JavaScript/experimentSix.js
@@ -0,0 +1,38 @@
+// var x = 20;
+// function fun(){
+//     console.log(x);
+//     var x = 24;
+// }
+
+// fun()
+
+
+// var x = 20;
+// console.log(x);
+// var x = 24;
+
+// async function fun(){
+//     return 'Hello'
+// }
+
+// fun().then(ans=> console.log(ans))
+
+// let x = 20;
+// function fun(){
+//     console.log(x);
+//     x = 24;
+// }
+
+// fun()
+
+// let hello = new Promise(()=>{
+
+// });
+
+// console.log(typeof hello)
+// console.log(typeof null)
+// console.log(typeof NaN)
+// console.log(typeof undefined)
+// console.log(typeof fun)
+// console.log(typeof [])
+// console.log(typeof {})
