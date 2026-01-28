59c1c0ad4adb7bb687887e82f3b2f07549d25d21
Aamir Khurshid <108178409+Aamir-Khurshid@users.noreply.github.com>
2026-01-28 18:25:54 +0530

    Add files via upload

---
+++ JavaScript/OOPS/index.js
@@ -0,0 +1,21 @@
+// function CreateBiscuit(name,price){
+//     this.name = name;
+//     this.price = price
+// }
+
+// let bisxcuit1 = new createBiscuit("ParleG",10)
+// console.log(bisxcuit1)
+
+// let bisxcuit2 = new createBiscuit("Parle",20)
+// console.log(bisxcuit2)
+
+
+class Aamir{
+    constructor(){
+
+    }
+}
+let ss = new Aamir()
+console.log(typeof Aamir)
+
+
